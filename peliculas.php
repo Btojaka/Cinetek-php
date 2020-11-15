3e6a58ab079ec4f62f6832f4141759cbc07eb278
Btojaka <bruthtoj@gmail.com>
2020-11-15 09:39:05 +0100

    Se eliminan etiquetas y se modifica codigo php

--- peliculas.php
+++ peliculas.php
@@ -20,29 +20,12 @@ include('lib/utils.php');
         <a href="./peliculas.php" class="btn btn-dark">Películas</a>&nbsp;&nbsp;
     </div>
     <div class="container">
-        <div class="row mx-auto">
-            <div class="portada">
-                <div class="botones">
-                    <button id="editar">Editar</button>
-                    <button id="borrar">Borrar</button>
-                </div>
-            </div>
-            
+        <div class="row mx-auto">           
             <!-- INCLUIR CÓDIGO PHP -->
             <?php
             // lee el fichero de peliculas
             $array = get_dataCsv("bbdd/peliculas.csv");
-            // var_dump($array);
-            // echo "<br>";
-            // echo "<br>";
-            // // retorna el ID y el Nombre de la pelicula en un array
-            // $arrayDatos = getIdyName($array);
-            // var_dump("id y nom: ".$arrayDatos);
-
-            
-            
-
-
+            get_Portadas($array);
             ?>
             </tbody>
         </table>
